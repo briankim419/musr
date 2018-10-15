63b309a1a4b94d7947147127c8dbfa33443aa0c4
Nicholas Carty <nickcarty13@gmail.com>
2018-10-15 14:24:05 -0400

    Created a genre table and model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_160924) do
+ActiveRecord::Schema.define(version: 2018_10_15_181005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
