b3b1a9a22bede8e80d963e46acb27ec460431dda
Nicholas Carty <nickcarty13@gmail.com>
2018-10-15 16:44:25 -0400

    fixed the rspec test

--- spec/features/01_visitors_sees_list_of_genres_spec.rb
+++ spec/features/01_visitors_sees_list_of_genres_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 feature "visitor sees a list of genres" do
+  let!(:rock) { Genre.create(name:'Rock') }
+  let!(:classical) { Genre.create(name: 'Classical') }
+
   scenario "Sees a list of genres and links for genres" do
-    rock = Genre.create(name:'Rock')
 
     visit genres_path
 
     expect(page).to have_content "Rock"
+    expect(page).to have_content "Classical"
 
   end
   scenario "clicks link and is taken to show page for given genre" do
-    rock = Genre.create(name:'Rock')
 
     visit genres_path
 
-    click_link "Rock"
+    expect(page).to have_link('Rock')
+    expect(page).to have_link('Classical')
 
-    expect(page).to have_content rock.name
   end
 end
