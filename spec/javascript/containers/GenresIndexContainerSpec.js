377241d4aa2184a20283a9893cb14d45e761939d
Nicholas Carty <nickcarty13@gmail.com>
2018-10-22 10:11:08 -0400

    WIP

--- spec/javascript/containers/GenresIndexContainerSpec.js
+++ spec/javascript/containers/GenresIndexContainerSpec.js
@@ -1,16 +1,15 @@
 import GenresIndexContainer from '../../../app/javascript/react/containers/GenresIndexContainer';
+import GenreTile from '../../../app/javascript/react/components/GenreTile'
 import fetchMock from 'fetch-mock'
 
-xdescribe('GenresIndexContainer', () => {
+describe('GenresIndexContainer', () => {
   let wrapper;
   let data;
 
   beforeEach(() => {
     data = {
       genres: [
-        {id: 1, name: 'Rock'},
-        {id: 2, name: 'Classical'},
-        {id: 3, name: 'Jazz'}
+        {id: 1, name: 'Rock'}
       ]
     }
     fetchMock.get('/api/v1/genres', {
@@ -23,17 +22,34 @@ xdescribe('GenresIndexContainer', () => {
   })
   afterEach(fetchMock.restore)
 
-  describe('header', () => {
+  describe('Index Container', () => {
     it('renders an h1', () => {
       expect(wrapper.find('h1')).toBePresent()
       expect(wrapper.find('h1').text()).toEqual('Genres')
     })
 
+    it('Should render a Genre Tile component', (done) => {
+      setTimeout(() => {
+        expect(wrapper.find('GenreTile')).toBePresent()
+        done()
+      }, 0)
+    })
+
+    it('renders the Genre Tile component with specific props', (done) => {
+      setTimeout(() => {
+        expect(wrapper.find('GenreTile').props()).toEqual({
+          id: 1,
+          name: 'Rock'
+        })
+        done()
+      }, 0)
+    })
+
+
+
     it('renders a p tag for each item returned from the api call', (done) => {
       setTimeout(() => {
         expect(wrapper.text()).toMatch(data.genres[0].name)
-        expect(wrapper.text()).toMatch(data.genres[1].name)
-        expect(wrapper.text()).toMatch(data.genres[2].name)
         done()
       }, 0)
     })
